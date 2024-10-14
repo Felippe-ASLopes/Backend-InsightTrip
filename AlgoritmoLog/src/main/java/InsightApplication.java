aa87358e29534fdcebd980b0776041d859ac509f
Felippe-ASLopes <felippedias3@gmail.com>
2024-10-14 09:40:54 -0300

    :sparkles: feat: Conexão com Bucket S3

--- AlgoritmoLog/src/main/java/InsightApplication.java
+++ AlgoritmoLog/src/main/java/InsightApplication.java
@@ -1,10 +1,12 @@
 import ETL.Leitor;
+import ETL.S3Provider;
 import Objetos.VooExterior;
 
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -13,6 +15,43 @@ import java.util.List;
 
 public class InsightApplication {
     public static void main(String[] args) {
+//        Conexão S3
+        S3Client s3Client = new S3Provider().getS3Client();
+//        S3Client s3 = S3Provider.getS3Client();
+        String bucketName = System.getenv("NOME_BUCKET");
+
+//        Listando objetos no bucket
+        try {
+            ListObjectsRequest listObjects = ListObjectsRequest.builder()
+                    .bucket(bucketName)
+                    .build();
+
+            List<S3Object> objects = s3Client.listObjects(listObjects).contents();
+            System.out.println("Objetos no bucket " + bucketName + ":");
+            for (S3Object object : objects) {
+                System.out.println("- " + object.key());
+            }
+        } catch (S3Exception e) {
+            System.err.println("Erro ao listar objetos no bucket: " + e.getMessage());
+        }
+
+//        Baixando arquivos
+        try {
+            List<S3Object> objects = s3Client.listObjects(ListObjectsRequest.builder().bucket(bucketName).build()).contents();
+            for (S3Object object : objects) {
+                GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(object.key())
+                        .build();
+
+                InputStream inputStream = s3Client.getObject(getObjectRequest, ResponseTransformer.toInputStream());
+                Files.copy(inputStream, new File(object.key()).toPath());
+                System.out.println("Arquivo baixado: " + object.key());
+            }
+        } catch (IOException | S3Exception e) {
+            System.err.println("Erro ao fazer download dos arquivos: " + e.getMessage());
+        }
+
 //    Extraindo dados do .xlsx
         String nomeArquivo = "chegadas_2023.xlsx";
         Path caminho = Path.of(nomeArquivo);
@@ -39,8 +78,6 @@ public class InsightApplication {
             e.printStackTrace();
         }
 
-//        Conexão S3
-        S3Client s3 = S3Provider.getS3Client();
-        String bucketName = System.getenv("NOME_BUCKET");
+
     }
 }
